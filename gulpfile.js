ab6f7eea0b4ffb947ccfaa7affbfcb44b5744634
Страхов Роман Владимирович <strakhovrv@iMac-002.local>
2018-11-21 18:25:55 +0300

    LiveReload made

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,61 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let watch = require('gulp-watch');
+let sass = require('gulp-sass');
+let changed = require('gulp-changed');
+
+let connect = require('gulp-connect');
+// let livereload = require('gulp-livereload');
+
+// var wait = require('gulp-wait');
+// var waitD1 = 5000;
+
+let distPath = 'dist';
+
+gulp.task('serve', function () {
+    connect.server({
+        name: 'Hello Wrold',
+        root: 'dist',
+        port: 8000,
+        livereload: true
+    });
+});
 
 gulp.task( 'watch', function() {
     gulp.watch( 'scss/**/*.scss', ['styles'] );
+    gulp.watch( 'js/**/*.js', ['scripts'] );
+    gulp.watch( 'index.html', ['views'] );
 } );
 
 gulp.task( 'styles', function() {
     return gulp.src('scss/*')
-        .pipe( sass() )
-        .pipe( gulp.dest('../css'));
+    .pipe( sass() )
+    .pipe( gulp.dest(distPath + '/css'))
+    .pipe( connect.reload() );        
 } );
 
-gulp.task( 'default', function() {
+gulp.task( 'views', function () {
+    return gulp.src( 'index.html' )
+    .pipe( gulp.dest(distPath + '/') )
+    .pipe( connect.reload() );        
+} );
+
+gulp.task( 'scripts', function () {
+    return gulp.src( 'js/**/*.js' ) // Matches 'client/js/somedir/somefile.js' and resolves `base` to `js/`
+    //       .pipe(minify())
+    //     .pipe( wait( waitD1 ) )
+    .pipe( gulp.dest(distPath + '/js') )
+    .pipe( connect.reload() );        
+} );
+
+
+gulp.task( 'reload', function () {
+    return ( connect.reload() );
+} );
+
+gulp.task( 'build', ['views', 'styles', 'scripts'], function() {
+    return 0;
+} );
+
+gulp.task( 'default', ['build', 'serve', 'watch'], function() {
     return 0;
 } );
