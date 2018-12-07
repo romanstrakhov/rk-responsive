c92302167493c4776a4669561cdb90f7290a4dee
Страхов Роман Владимирович <strakhovrv@iMac-002.local>
2018-12-07 08:27:49 +0300

    Workspace update (eslint, gulp)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
+    rules: {
+        'no-console': 'off',
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    },
     "extends": "airbnb-base"
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,45 @@
 const gulp = require('gulp');
-const watch = require('gulp-watch');
+// const watch = require('gulp-watch');
 const sass = require('gulp-sass');
-const changed = require('gulp-changed');
-
+// const changed = require('gulp-changed');
 const connect = require('gulp-connect');
 // let livereload = require('gulp-livereload');
-
 // var wait = require('gulp-wait');
 // var waitD1 = 5000;
 
 const distPath = 'dist';
 
-gulp.task('serve', function () {
-    connect.server({
-        name: 'Hello Wrold',
-        root: 'dist',
-        port: 8000,
-        livereload: true
-    });
+gulp.task('serve', () => {
+  connect.server({
+    name: 'Hello World',
+    root: 'dist',
+    port: 8000,
+    livereload: true,
+  });
 });
 
-gulp.task( 'watch', function() {
-    gulp.watch( 'scss/**/*.scss', ['styles'] );
-    gulp.watch( 'js/**/*.js', ['scripts'] );
-    gulp.watch( 'index.html', ['views'] );
-} );
+gulp.task('watch', () => {
+  gulp.watch('scss/**/*.scss', gulp.series('styles'));
+  gulp.watch('js/**/*.js', gulp.series('scripts'));
+  gulp.watch('index.html', gulp.series('views'));
+});
 
-gulp.task( 'styles', function() {
-    return gulp.src('scss/*')
-    .pipe( sass() )
-    .pipe( gulp.dest(distPath + '/css'))
-    .pipe( connect.reload() );        
-} );
+gulp.task('styles', () => gulp.src('scss/*')
+  .pipe(sass())
+  .pipe(gulp.dest(`${distPath}/css`))
+  .pipe(connect.reload()));
 
-gulp.task( 'views', function () {
-    return gulp.src( 'index.html' )
-    .pipe( gulp.dest(distPath + '/') )
-    .pipe( connect.reload() );        
-} );
+gulp.task('views', () => gulp.src('index.html')
+  .pipe(gulp.dest(`${distPath}/`))
+  .pipe(connect.reload()));
 
-gulp.task( 'scripts', function () {
-    return gulp.src( 'js/**/*.js' ) // Matches 'client/js/somedir/somefile.js' and resolves `base` to `js/`
-    //       .pipe(minify())
-    //     .pipe( wait( waitD1 ) )
-    .pipe( gulp.dest(distPath + '/js') )
-    .pipe( connect.reload() );        
-} );
+gulp.task('scripts', () => gulp.src('js/**/*.js') // Matches 'client/js/somedir/somefile.js' and resolves `base` to `js/`
+  //       .pipe(minify())
+  //     .pipe( wait( waitD1 ) )
+  .pipe(gulp.dest(`${distPath}/js`))
+  .pipe(connect.reload()));
 
 
-gulp.task( 'reload', function () {
-    return ( connect.reload() );
-} );
-
-gulp.task( 'build', ['views', 'styles', 'scripts'], function() {
-    return 0;
-} );
-
-gulp.task( 'default', ['build', 'serve', 'watch'], function() {
-    return 0;
-} );
+gulp.task('reload', () => (connect.reload()));
+gulp.task('build', gulp.parallel('views', 'styles', 'scripts'), () => 0);
+gulp.task('default', gulp.parallel('build', 'serve', 'watch'), () => 0);
